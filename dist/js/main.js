e1ad732cb978696bd173ec83d48480198396a0f3
Vladimir Dmitriev <defurator@mail.ru>
2015-12-30 19:48:35 +0200

    dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,4 +12,70 @@ $(document).ready(function(){
   $(window).resize(function() {
     heightDetect();
   });
+  $('.clients_list').bxSlider({
+    slideWidth: 181,
+    minSlides: 1,
+    maxSlides: 5,
+    moveSlides: 1,
+    pager: false,
+    auto: true,
+    slideMargin: 45,
+    pause: 5000,
+    autoHover: true,
+    easing: 'linear'
+  });
+  /************************************************************************/
+  var map;
+  var myLatlng = new google.maps.LatLng(53.907179, 27.484561);
+  function initialize() {
+    var styles = [
+      /*{
+        stylers: [
+          { hue: "#cccccc" },
+          { saturation: -120 }
+        ]
+      },{
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { lightness: 100 },
+          { visibility: "simplified" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }*/
+    ];
+    var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+    var mapOptions = {
+      zoom: 15,
+      //center: new google.maps.LatLng(53.905497, 27.558681)
+      center: myLatlng,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      }
+    };
+    map = new google.maps.Map(document.getElementById('footer'),
+        mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"Webformat"
+    });
+    marker.setMap(map);
+    var contentString = '<div class="main9_wrap"><img src="http://landing.wfs.by/sites/all/themes/landing/image/webformat.png" alt="" /><div class="main9_text">+375 44 732 05 09<br>г.Минск Притыцкого 29 оф 522</div></div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    infowindow.open(map,marker);
+
+
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+  /************************************************************************/
 });
